70106fb83af5cc6de8b9f5c10ce26cc6b8d71f01
Alana Cristina Carvalho Araujo <alana.araujo35@gmail.com>
2020-08-07 20:12:56 -0300

    Quarta-aula da Semana do NLW - parte 1

---
+++ src/server.js
@@ -0,0 +1,109 @@
+const proffys = [//cada professor é um objeto, por causa das "{}", dentro de uma lista de objetos, por causa dos "[]", chamada proffys
+    {
+        name: "Diego Fernandes", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "981283274", bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: "20,00", 
+        weekday: [
+            0
+        ], time_from: [720],
+        time_to: [1220]
+    },
+
+    {
+        name: "Danielle Evangelista", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "981283274", bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: "20,00", 
+        weekday: [
+            1
+        ], time_from: [720],
+        time_to: [1220]
+    },
+
+    {
+        name: "Mayk Brito", 
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4", 
+        whatsapp: "981283274", bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: "20,00", 
+        weekday: [
+            1
+        ], time_from: [720],
+        time_to: [1220]
+    }
+]
+
+const subjects = [
+    "Artes", 
+    "Biologia", 
+    "Ciências", 
+    "Educação física", 
+    "Física", 
+    "Geografia", 
+    "História", 
+    "Matemática", 
+    "Português", 
+    "Química", 
+]
+/*
+function pageStudy(req, res) {
+    return res.sendFile(__dirname + "/views/study.html")
+}
+
+function pageGiveClasses(req, res) {
+    return res.sendFile(__dirname + "/views/give-classes.html")
+}*/
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+
+function pageStudy(req, res) {
+    const filters = req.query
+    return res.render("study.html", {proffys, filters})
+}
+
+function pageGiveClasses(req, res) {
+    return res.render("give-classes.html")
+}
+
+//CONFIGURAR EXPRESS
+const express = require('express')
+const server = express()
+
+
+//CONFIGURAR NUNJUCKS
+//com o nunjucks eu consigo enviar a lista de objetos, proffys
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+
+server
+
+//  CONFIGURAR ARQUIVOS ESTÁTICOS (CSS, SCRIPTS, IMAGENS)
+.use(express.static("public"))
+//tudo que está na pasta public ssão considerados arquivos static
+
+//tem duas formas de fazer esse ".get", pode ser colocando a função junto com o comando com esta a baixo, ou separando a função como mostrado nas duas páginas seguintes
+
+//ROTAS DA APLICAÇÃO
+/*.get("/", (req, res) => {//é uma função que não precisa de nome,então ao invés de eu colocar a palavra "function" antes eu só coloco a setinha depois do parenteses
+// // // req = requisição e res = resposta
+    return res.sendFile(__dirname + "/views/index.html") //__dirname = C:\NLW\src
+})*/
+
+.get("/", pageLanding)
+
+.get("/study", pageStudy)//No ".get" o primeiro campo é sobre como vai estar o html, nessa caso "http://127.0.0.1:5500/study" e o segundo é a função do que vai mostrar quando estiver nessa página
+
+.get("/give-classes", pageGiveClasses)
+
+.listen(5500)//express é uma função por isso precisa dos parenteses
+// // //a gente abriu a porta 5500,quando colocamos ali em cima, então se eu tentar acessar com outra porta, tipo 5501, eu não conseguiria
+
+//console.log(__dirname) -> ao colocar o console log, o print vai aparecer noo terminal, não no console log no inspecionar da página.
